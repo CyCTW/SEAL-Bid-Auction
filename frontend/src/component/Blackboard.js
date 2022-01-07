c168f8c56486c807abc3598a6feb9bb8b99fef82
MaureenM2001 <MaureenM8803@gmail.com>
2022-01-07 09:38:49 +0800

    Blackboard button

--- frontend/src/component/Blackboard.js
+++ frontend/src/component/Blackboard.js
@@ -3,6 +3,7 @@ import React from 'react'
 const Blackboard = () => (
     <div class="ui inverted segment">
         <h3>Black Board</h3>
+        <button class="position-absolute bottom-0 end-0 ui green button">Show Proof</button>
     </div>
 )
 
