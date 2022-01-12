e08701febf8de309fa2b8ee294dfd7954fbe7149
kaiiiz <ukaizheng@gmail.com>
2022-01-12 23:43:01 +0800

    Create auction

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,40 +17,39 @@ function App() {
       let accounts = await web3.eth.getAccounts();
 
       web3.eth.defaultAccount = accounts[0];
-      setAccount(accounts[0])
+      setAccount(accounts[0]);
 
-      getAuctions().then(
-        (data) => {
-          for(let i=0; i<data.length; i++) {
-            let d = data[i]
-            const expired_date = new Date(d.expired_date)
-            // expired_date.setHours(expired_date.getHours() + 8)
-            data[i].expired_date = expired_date.toString()
-          }
-          setAuctions(data)
-          console.log(data)
-        }
-      ).catch((err) => {
-        console.log(err)
-      })
+      try {
+        let data = await getAuctions();
+
+        // for (let i = 0; i < data.length; i++) {
+        //   let d = data[i];
+        //   console.log(d);
+        //   // const expired_date = new Date(d.expired_date)
+        //   // expired_date.setHours(expired_date.getHours() + 8)
+        //   // data[i].expired_date = expired_date.toString()
+        // }
+        setAuctions(data);
+      } catch (err) {
+        console.log(err);
+      }
     }
-
     func();
   }, [])
-  
-  return (
-      <Router>
-        <NavBar account={account} setAccount={setAccount}/>
 
-        <Switch>
-          <Route path="/:auctionId">
-            <Auction />
-          </Route>
-          <Route path="/">
-            {auctions && <Home auctions={auctions}/>}
-          </Route>
-        </Switch>
-      </Router>
+  return (
+    <Router>
+      <NavBar account={account} setAccount={setAccount} />
+
+      <Switch>
+        <Route path="/:auctionId">
+          <Auction />
+        </Route>
+        <Route path="/">
+          {auctions && <Home auctions={auctions} />}
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,11 +1,12 @@
 import axios from "axios";
+import { init_schnorr_group } from "./component/zk-proof/utils"
 
 import mainContractBuild from "./contracts/Main.json"
 
 // const hostname = "http://localhost:3001";
 // const path = "api/auctions";
 
-const mainContractAddress = "0x02329246841153c4e574e4d6d3462f4987FbC170"
+const mainContractAddress = "0x3d508e6ea8AEff06d2c8740Ab12438023d8C5D37"
 const mainContractABI = mainContractBuild['abi']
 
 const web3 = new Web3("ws://localhost:7545");
@@ -25,7 +26,21 @@ export const getAuction = async (auctionID) => {
 };
 
 export const addAuction = async (data) => {
-  console.log("add", data);
-  const res = await axios.post(`${hostname}/${path}`, data);
-  return res;
+  let auctionName = data['name'];
+  let auctionDesc = data['description'];
+  let expiredDate = data['expired_date'];
+  let relCommitDeadlineSec = Math.floor((expiredDate - Date.now()) / 1000);
+  let { g, p, q } = await init_schnorr_group()
+  console.log(g, p, q);
+
+  let r = await mainContract.methods.createAuction(
+    auctionName, auctionDesc, relCommitDeadlineSec, p, q, g)
+    .send({
+      'from': web3.eth.defaultAccount,
+      'gas': 5000000,
+      'gasPrice': 5000000,
+    })
+  console.log(r);
+  // const res = await axios.post(`${hostname}/${path}`, data);
+  // return res;
 };
