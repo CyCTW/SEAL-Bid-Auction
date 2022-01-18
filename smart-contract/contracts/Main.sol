19606f67b12f73fdd459b9a0e87911e5c5982839
kaiiiz <ukaizheng@gmail.com>
2022-01-18 21:28:50 +0800

    Update demo version contract

--- smart-contract/contracts/Main.sol
+++ smart-contract/contracts/Main.sol
@@ -3,16 +3,9 @@ pragma solidity >=0.4.22 <0.9.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-// Improve: Use Event&Log to store Bidder information
 contract Bidder is Ownable {
     Auction public auctionAddr;
     string public commitment; // JSON
-    string[] public r1; // JSON (start from MSB)
-    string[] public r2; // JSON (start from MSB)
-
-    // uint256[] public gx; // round 1 (start from MSB)
-    // uint256[] public gr; // round 1 (start from MSB)
-    // uint256[] public b; // round 2 (start from MSB)
 
     constructor(
         Auction _auctionAddr,
@@ -20,19 +13,6 @@ contract Bidder is Ownable {
     ) {
         auctionAddr = _auctionAddr;
         commitment = _commitment;
-    }
-
-    function round1(string memory _r1) public onlyOwner {
-        r1.push(_r1);
-        // TODO: check already submit case
-        // gx.push(_gx);
-        // gr.push(_gr);
-    }
-
-    function round2(string memory _r2) public onlyOwner {
-        r2.push(_r2);
-        // TODO: check already submit case
-        // b.push(_b);
     }
 }
 
@@ -51,17 +31,10 @@ contract Auction {
     uint256 public p;
     uint256 public g;
 
-    // state maintain parameters
-    // bool public mpcStartFlag = false;
-    // uint8 public curBit = bitLen - 1;
-    // uint8 public curRound = 1;
-    // mapping(uint8 => mapping(uint8 => address[])) public roundBidders;
-
     event JoinAuctionEvent(address sender, string commitment);
     event Round1Event(address sender, string r1);
     event Round2Event(address sender, string r2);
     event claimWinnerEvent(address sender, string prik);
-    // event RoundEndNotification(uint8 curBit, uint8 curRound);
 
     uint256 public commitDeadline;
 
@@ -83,41 +56,9 @@ contract Auction {
         g = _g;
     }
 
-    // modifier mpcStart() {
-    //     require(
-    //         mpcStartFlag == true ||
-    //             (mpcStartFlag == false && block.timestamp > commitDeadline)
-    //     );
-
-    //     // start auction
-    //     if (mpcStartFlag == false && block.timestamp > commitDeadline) {
-    //         mpcStartFlag = true;
-    //     }
-    //     _;
-    // }
-
-    // modifier updateState() {
-    //     _;
-    //     roundBidders[curBit][curRound].push(msg.sender);
-    //     // move to next round
-    //     if (roundEnd()) {
-    //         if (curRound == 1) {
-    //             curRound = 2;
-    //         } else if (curRound == 2) {
-    //             curBit--;
-    //             curRound = 1;
-    //         }
-    //         // emit RoundEndNotification(curBit, curRound);
-    //     }
-    // }
-
     function getBidders() public view returns (address[] memory) {
         return bidders;
     }
-
-    // function roundEnd() public view returns (bool) {
-    //     return roundBidders[curBit][curRound].length == bidders.length;
-    // }
 
     function joinAuction(string memory commitment) public returns (Bidder) {
         require(
@@ -134,14 +75,10 @@ contract Auction {
     }
 
     function round1(string memory _r1) public {
-        // require(curRound == 1, "Not round 1");
-        // bidderContractMap[msg.sender].round1(_r1);
         emit Round1Event(msg.sender, _r1);
     }
 
     function round2(string memory _r2) public {
-        // require(curRound == 2, "Not round 2");
-        // bidderContractMap[msg.sender].round2(_r2);
         emit Round2Event(msg.sender, _r2);
     }
 
